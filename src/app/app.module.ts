4811496360ed5be30563ea32be76db2b9ea29390
quandang96 <quandanght@gmail.com>
2021-07-22 16:52:09 +0700

    add nav and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,25 @@ import { NavComponent } from './layout/nav/nav.component';
 import { LayoutModule } from './layout/layout.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +29,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    HomeModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+  @ViewChildren('slides') slides!: QueryList<ElementRef>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // constructor() { }
+  ngAfterViewInit(): void {
   }
 
+  // ngOnInit(): void {
+    
+  // }
+
+//   plusSlides(n: number){
+//     let slideIndex = 1 + n;
+//     let slide = document.getElementsByClassName("mySlides");
+//     if (n > slide.length) {slideIndex = 1}    
+//     if (n < 1) {slideIndex = slide.length}
+//     slideIndex++;
+//     for (let i = 0; i < slide.length; i++) {
+//       slide[i].style.display= "none";  
+//   }
+//   slide[slideIndex-1].style.display = "block";
+// }
+  
 }
+
